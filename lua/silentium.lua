362a54af691a1fcba135d8959160c8e10ab25399
Artem Krinitsyn <arkriny@arkriny.com>
2026-07-14 16:15:42 +0000

    fix: syntax_on check is always true
    Firstly, has() and exists() return 0/1, so the condition is always true in Lua.
    
    Secondly, has() checks feature support, but what we want is to check
    whether 'syntax_on' is defined.

--- lua/silentium.lua
+++ lua/silentium.lua
@@ -44,7 +44,7 @@ function M.colorscheme()
 	vim.o.background = "dark"
 	vim.g.colors_name = "silentium"
 	vim.cmd.highlight("clear")
-	if vim.fn.has("syntax_on") then
+	if vim.fn.exists("syntax_on") == 1 then
 		vim.cmd.syntax("reset")
 	end
 
